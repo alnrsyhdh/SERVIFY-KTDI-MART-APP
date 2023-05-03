89ee82199b337c5b5e23c773dbb116fe824246a3
Alia Nur Syuhadah <76759043+alnrsyhdh@users.noreply.github.com>
2023-05-03 08:42:03 +0800

    ADMIN REGISTER AND LOGIN
    default test acc password : Test12345.

--- app/src/main/java/com/example/servify/AdminLogin.java
+++ app/src/main/java/com/example/servify/AdminLogin.java
@@ -1,14 +1,112 @@
 package com.example.servify;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AdminLogin extends AppCompatActivity {
+
+    private TextView tv_login_movesignup;
+    private EditText et_login_email, et_login_password;
+    private Button bt_login_login;
+    private ProgressDialog progressDialog;
+
+    FirebaseAuth nAuth;
+    FirebaseUser nUser;
+
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+
+        tv_login_movesignup=findViewById(R.id.tv_login_movesignup);
+        bt_login_login=findViewById(R.id.bt_login_login);
+        et_login_email=findViewById(R.id.et_login_email);
+        et_login_password=findViewById(R.id.et_login_password);
+        progressDialog = new ProgressDialog(this);
+        nAuth=FirebaseAuth.getInstance();
+        nUser=nAuth.getCurrentUser();
+
+        tv_login_movesignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdminLogin.this, AdminRegister.class));
+            }
+        });
+
+        bt_login_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PerforLog();
+            }
+        });
+
+
+    }
+
+    private void PerforLog() {
+
+        String email = et_login_email.getText().toString();
+        String password = et_login_password.getText().toString();
+
+        if(!email.matches(emailPattern))
+        {
+            et_login_email.setError("Enter Correct Email!");
+        }
+
+        else if(password.isEmpty() || password.length()<8)
+        {
+            et_login_password.setError("Enter Correct Password!");
+        }
+
+        else
+        {
+            progressDialog.setMessage("Please wait while login...");
+            progressDialog.setTitle("Login...");
+            progressDialog.show();
+
+            nAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(AdminLogin.this, "Login Succesfull", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        progressDialog.dismiss();
+                        Toast.makeText(AdminLogin.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+
     }
 }
+
+    private void sendUserToNextActivity() {
+        Intent intent = new Intent(AdminLogin.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    }
--- app/src/main/java/com/example/servify/AdminRegister.java
+++ app/src/main/java/com/example/servify/AdminRegister.java
@@ -1,14 +1,117 @@
 package com.example.servify;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AdminRegister extends AppCompatActivity {
+
+    private TextView tv_register_movelogin;
+    private EditText et_resgister_email, et_register_pwd, et_register_cf_pwd;
+    private Button bt_register_submit;
+    ProgressDialog progressDialog;
+
+    FirebaseAuth nAuth;
+    FirebaseUser nUser;
+
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_register);
+
+        tv_register_movelogin=findViewById(R.id.tv_register_movelogin);
+        et_resgister_email=findViewById(R.id.et_resgister_email);
+        et_register_pwd=findViewById(R.id.et_register_pwd);
+        bt_register_submit=findViewById(R.id.bt_register_submit);
+        et_register_cf_pwd=findViewById(R.id.et_register_cf_pwd);
+        progressDialog = new ProgressDialog(this);
+        nAuth=FirebaseAuth.getInstance();
+        nUser=nAuth.getCurrentUser();
+
+       tv_register_movelogin.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               startActivity(new Intent(AdminRegister.this, AdminLogin.class));
+           }
+       });
+
+       bt_register_submit.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               PerforAuth();
+           }
+       });
+
+    }
+
+    private void PerforAuth() {
+        String email = et_resgister_email.getText().toString();
+        String password = et_register_pwd.getText().toString();
+        String confirmPassword = et_register_cf_pwd.getText().toString();
+
+        if(!email.matches(emailPattern))
+        {
+            et_resgister_email.setError("Enter Correct Email!");
+        }
+
+        else if(password.isEmpty() || password.length()<8)
+        {
+            et_register_pwd.setError("Enter Proper Password!");
+        }
+
+        else if(!password.equals(confirmPassword))
+        {
+            et_register_cf_pwd.setError("Password did not match!");
+        }
+        else
+        {
+            progressDialog.setMessage("Please wait while registeration...");
+            progressDialog.setTitle("Registeration...");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            nAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(AdminRegister.this, "Registeration Succesfull", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        progressDialog.dismiss();
+                        Toast.makeText(AdminRegister.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+
+
+
+    }
+
+    private void sendUserToNextActivity() {
+        Intent intent = new Intent(AdminRegister.this, AdminLogin.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
