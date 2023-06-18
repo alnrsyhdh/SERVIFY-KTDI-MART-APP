85dec90c20bacd8a33085f931142734c2dbbb4cf
Masyitah binti Mustapha <masyitah1505@gmail.com>
2023-06-18 12:43:47 +0800

    Retrieve data from products & wishlist (user & admin)
    - add recycler view in both sides

--- app/src/main/java/com/example/servify/admin/AdminFirstFragment.java
+++ app/src/main/java/com/example/servify/admin/AdminFirstFragment.java
@@ -13,8 +13,14 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.servify.R;
+import com.example.servify.customer.ProductAdapter;
+import com.example.servify.customer.Wish;
+import com.example.servify.customer.WishAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -26,8 +32,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdminFirstFragment extends Fragment {
     private Spinner statusDropdown;
+    private RecyclerView recyclerView, recyclerView2;
+    private ProductAdapter productAdapter;
+    private List<Product> productList;
+    private List<Wish> wishList;
 
     DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference("servify/status/shop_status");
 
@@ -41,7 +54,95 @@ public class AdminFirstFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View admin_firstfragment = inflater.inflate(R.layout.admin_fragment_first, container, false);
+        recyclerView = admin_firstfragment.findViewById(R.id.recyclerView);
+
+        // Set up the layout manager for the RecyclerView
+        LinearLayoutManager horizontalLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(horizontalLayoutManager);
+
+        // Initialize the productList
+        productList = new ArrayList<>();
+
+        // Set up the adapter with the productList
+        productAdapter = new ProductAdapter(productList);
+        recyclerView.setAdapter(productAdapter);
+
+        // Retrieve data from Firebase and set up the initial RecyclerView (product)
+        loadProducts();
+
+        recyclerView2 = admin_firstfragment.findViewById(R.id.recyclerView2);
+
+        // Initialize the wishList
+        wishList = new ArrayList<>();
+
+        // Set up the adapter with the wishList
+        WishAdapter wishAdapter = new WishAdapter(wishList);
+        recyclerView2.setAdapter(wishAdapter);
+
+        // Set up the layout manager for the RecyclerView
+        LinearLayoutManager horizontalLayoutManager2 = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView2.setLayoutManager(horizontalLayoutManager2);
+
+        // Retrieve data from Firebase and set up the initial RecyclerView (wish)
+        loadProducts2();
+
         return admin_firstfragment;
+    }
+
+    private void loadProducts2() {
+        DatabaseReference wishRef = FirebaseDatabase.getInstance().getReference("servify/wishForm");
+        wishRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                wishList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    int wishId = snapshot.child("id").getValue(Integer.class);
+                    String wishPic = snapshot.child("imageUrl").getValue(String.class);
+                    String wishName = snapshot.child("uName").getValue(String.class);
+                    String wishDetails = snapshot.child("pName").getValue(String.class);
+
+                    Wish wish = new Wish(wishId, wishName, wishDetails, wishPic);
+                    wishList.add(wish);
+                }
+
+                // Initialize the adapter with the updated productList
+                WishAdapter wishAdapter = new WishAdapter(wishList);
+                recyclerView2.setAdapter(wishAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle the error case if retrieval is canceled
+            }
+        });
+    }
+
+    private void loadProducts() {
+        DatabaseReference productsRef = FirebaseDatabase.getInstance().getReference("servify/products");
+        productsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                productList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    int productId = snapshot.child("productId").getValue(Integer.class);
+                    String productName = snapshot.child("productName").getValue(String.class);
+                    String productPic = snapshot.child("productPic").getValue(String.class);
+                    double productPrice = snapshot.child("productPrice").getValue(Double.class);
+
+                    Product productDisplay = new Product(productId, productName, productPrice, productPic);
+                    productList.add(productDisplay);
+                }
+
+                // Initialize the adapter with the updated productList
+                productAdapter = new ProductAdapter(productList);
+                recyclerView.setAdapter(productAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle the error case if retrieval is canceled
+            }
+        });
     }
 
     public void onViewCreated(View admin_firstfragment, Bundle savedInstanceState) {
--- app/src/main/java/com/example/servify/customer/FirstFragment.java
+++ app/src/main/java/com/example/servify/customer/FirstFragment.java
@@ -1,29 +1,35 @@
 package com.example.servify.customer;
 
-import static android.content.ContentValues.TAG;
-
-import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.servify.R;
+import com.example.servify.admin.Product;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class FirstFragment extends Fragment {
+
+    private RecyclerView recyclerView3, recyclerView4;
+    private ProductAdapter productAdapter;
+    private List<Product> productList;
+    private List<Wish> wishList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,6 +39,39 @@ public class FirstFragment extends Fragment {
 
         DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference("servify/status/shop_status");
         TextView statusTv = first_view.findViewById(R.id.statusTv);
+
+        recyclerView3 = first_view.findViewById(R.id.recyclerView3);
+
+        // Set up the layout manager for the RecyclerView
+        LinearLayoutManager horizontalLayoutManager3 = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView3.setLayoutManager(horizontalLayoutManager3);
+
+        // Initialize the productList
+        productList = new ArrayList<>();
+
+        // Set up the adapter with the productList
+        productAdapter = new ProductAdapter(productList);
+        recyclerView3.setAdapter(productAdapter);
+
+        // Retrieve data from Firebase and set up the initial RecyclerView (product)
+        loadProducts3();
+
+        recyclerView4 = first_view.findViewById(R.id.recyclerView4);
+
+        // Initialize the wishList
+        wishList = new ArrayList<>();
+
+        // Set up the adapter with the wishList
+        WishAdapter wishAdapter = new WishAdapter(wishList);
+        recyclerView4.setAdapter(wishAdapter);
+
+        // Set up the layout manager for the RecyclerView
+        LinearLayoutManager horizontalLayoutManager4 = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView4.setLayoutManager(horizontalLayoutManager4);
+
+        // Retrieve data from Firebase and set up the initial RecyclerView (wish)
+        loadProducts4();
+
         databaseRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -65,4 +104,61 @@ public class FirstFragment extends Fragment {
 
         return first_view;
     }
+
+    private void loadProducts3() {//products
+        DatabaseReference productsRef = FirebaseDatabase.getInstance().getReference("servify/products");
+        productsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                productList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    int productId = snapshot.child("productId").getValue(Integer.class);
+                    String productName = snapshot.child("productName").getValue(String.class);
+                    String productPic = snapshot.child("productPic").getValue(String.class);
+                    double productPrice = snapshot.child("productPrice").getValue(Double.class);
+
+                    Product productDisplay = new Product(productId, productName, productPrice, productPic);
+                    productList.add(productDisplay);
+                }
+
+                // Initialize the adapter with the updated productList
+                productAdapter = new ProductAdapter(productList);
+                recyclerView3.setAdapter(productAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle the error case if retrieval is canceled
+            }
+        });
+    }
+
+    private void loadProducts4() {//wish
+        DatabaseReference wishRef = FirebaseDatabase.getInstance().getReference("servify/wishForm");
+        wishRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                wishList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    int wishId = snapshot.child("id").getValue(Integer.class);
+                    String wishPic = snapshot.child("imageUrl").getValue(String.class);
+                    String wishName = snapshot.child("uName").getValue(String.class);
+                    String wishDetails = snapshot.child("pName").getValue(String.class);
+
+                    Wish wish = new Wish(wishId, wishName, wishDetails, wishPic);
+                    wishList.add(wish);
+                }
+
+                // Initialize the adapter with the updated productList
+                WishAdapter wishAdapter = new WishAdapter(wishList);
+                recyclerView4.setAdapter(wishAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle the error case if retrieval is canceled
+            }
+        });
+    }
+
 }
